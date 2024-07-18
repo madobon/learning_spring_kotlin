e384734915c37c4ad7b775176eb2947725f68ec0
madobon <mimicopy@live.jp>
2024-07-19 08:54:23 +0900

    init

---
+++ src/main/kotlin/com/example/its_kotlin/ItsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.its_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItsKotlinApplication>(*args)
+}
